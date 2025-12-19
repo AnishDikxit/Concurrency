cbc5d08227883578d65ff2523f9ecea92d7174c0
AnishDikxit <anish.22053@gmail.com>
2025-12-19 18:45:29 +0530

    Multithreading

--- test.cpp
+++ test.cpp
@@ -10,6 +10,12 @@ int main(){
     std::vector<std::thread> threads;
     for(int i = 0; i<10; i++){
         threads.push_back(std::thread(hello, i));
+        //Immediately calling join is causing the code to wait for currently launched thread to finish execution
+        //the reason why id are same is
+        //the same id is getting recycled, meaning reused by consecutive threads
+        // threads[i].join();
+    }
+    for(int i = 0; i<10; i++){
         threads[i].join();
     }
     std::cout<<"Hello from main"<<'\n';
